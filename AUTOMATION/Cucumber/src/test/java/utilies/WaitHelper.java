c6533f66ee0ff4af07790327bf70e0d49d5964d5
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2023-12-26 19:58:31 +0530

    Bala_26Dec'23

--- AUTOMATION/Cucumber/src/test/java/utilies/WaitHelper.java
+++ AUTOMATION/Cucumber/src/test/java/utilies/WaitHelper.java
@@ -16,9 +16,14 @@ public class WaitHelper {
 		this.driver = driver;
 	}
 	
-
+	
 	public void WaitForElement(WebElement element, long timeOutInSeconds) {
 		Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSeconds));
 		wait.until(ExpectedConditions.visibilityOf(element));
 	}
 }
+
+//Get - to get the data
+//Post - to insert the data  
+//Put - to update the data 
+//Delete - to delete the data
