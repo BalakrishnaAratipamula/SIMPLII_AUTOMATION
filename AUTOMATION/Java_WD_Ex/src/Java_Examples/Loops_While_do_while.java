a00f82db4d176c620e83164425973c2921a616c8
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-01-30 21:17:36 +0530

    Bala_30Jan'24

--- AUTOMATION/Java_WD_Ex/src/Java_Examples/Loops_While_do_while.java
+++ AUTOMATION/Java_WD_Ex/src/Java_Examples/Loops_While_do_while.java
@@ -6,21 +6,20 @@ public class Loops_While_do_while {
 		/*/while
 		int n=1;
 		while(n<=5) {
-			System.out.println(n+ " a");
+			System.out.println(n);
 			n++;
-		} */
+		} /*/
 
 
-		/*/do-while
+		//do-while
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter n value: ");
 		int n = sc.nextInt();
 		//same as while loop but it will allow to execute at least one time inside the block of statement irrespective of condition true / false
 		do {
-			System.out.println(n+ " bala");
+			System.out.println(n);
 			++n;
-		}while(n<=5); */
+		}while(n<=5);
 
-
-	}
+	} //
 }
