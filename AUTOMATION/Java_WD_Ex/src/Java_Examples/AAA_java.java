4ec1b938035865ff388453b76fd89ac2a3508458
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-02-15 19:13:59 +0530

    Bala_15Feb'24

--- AUTOMATION/Java_WD_Ex/src/Java_Examples/AAA_java.java
+++ AUTOMATION/Java_WD_Ex/src/Java_Examples/AAA_java.java
@@ -20,19 +20,19 @@ public static void main(String[] args) {
 	
 	
 	
-//--------------------fibonacci numbers print based on given value
+/*/--------------------fibonacci numbers print based on given value
 public static void main(String[] args) {
-	int a=0;
-	int b=1;
+	int a=0
+	b=1,
+	c=a+b;
 	System.out.print(a+", "+b+", ");
-	int c=a+b;
 	while(c<=100) {
 		System.out.print(c+", ");
 		//to swap the values
 		a=b;
 		b=c;
 		c=a+b;
-	}
+	} */
 	
 	
 	
@@ -43,30 +43,37 @@ public static void main(String[] args) {
 	    	c=a+b;
 	        a=b;
 	  		b=c;
-	        System.out.print(" ,"+c);   
-	        subMethod(count-1);    
+	        System.out.print(c+", ");   
+	        subMethod(count-1); 
 	     }    
 	 }    
 	 
 	 public static void main(String args[]){    
 	 int count=5;    
-	 System.out.print(a+" "+b); //printing 0 and 1 & it will call one time only
+	 System.out.print(a+", "+b+", "); //printing 0 and 1 & it will call one time only
 	 subMethod(count-2); //because 2 numbers (a & b) are already printed so in total counts reduced by 2 times &&& //count must declare while calling subMethod
 	 */
-	
+
 	
 	
 	 /*/-----------------factorial for given given number
-	 Scanner sc = new Scanner(System.in);
-	 System.out.println("Enter any number: ");
-	 int n = sc.nextInt();
-	 int fact=1;
-	 for(int i=1; i<=n; i++) {
-		 fact = fact*i;
-	 }
-	 System.out.println("The factorial of " +n+ " is: "+ fact);
-	sc.close(); */
-
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter any number: ");
+	int n = sc.nextInt();
+	int fact=1;
+	for(int i=1; i<=n; i++) {
+		if(n==0) {
+			System.out.println("The factorial of " +n+ " is: "+ 1);
+		}
+		else {
+			fact = fact*i;
+		}
+	}
+	System.out.println("The factorial of " +n+ " is: "+ fact);
+	sc.close(); 
+	*/
+	
 	/*/factorial program in java using recursion
 	public static int subMethod(int n){    //(or) static int factorial(int n){
 		  if(n==0) {    
@@ -79,27 +86,31 @@ public static void main(String[] args) {
 	public static void main(String args[]){ 
 		int n=3;
 		int fact=1;
-    fact = subMethod(n);   //or num &&& //count must declare while calling sunMethod
+		fact = subMethod(n);   //or num &&& //count must declare while calling sunMethod
     
-    System.out.println("Factorial of "+n+" is: "+fact); //or num   */
-    
+    System.out.println("Factorial of "+n+" is: "+fact); //or num   
+    */
 	
 	
  	
 	/*/-----------------------to find number of occurrence of "char"
-	String str = "LiveTech e";
-	int count=0;
-	for(int i=0; i<=str.length()-1; i++) {
-		if(str.charAt(i)=='e') {
-			count++;
+	public static void main(String[] args) {
+		String str = "LiveTech EE";
+		int count=0;
+		String ucStr = str.toLowerCase();
+		System.out.println(ucStr);
+		for(int i=0; i<=ucStr.length()-1; i++) {
+			if(ucStr.charAt(i)=='e') {	//comparing with lower letter take toLowerCase() or comparing with higher letter take toUpperCase()
+				count++;
+			}
 		}
-	}
-	System.out.println("occurence of e is: "+count);  */
-	
+		System.out.println("occurence of e is: "+count); 
+	*/
 	
 	
 	
 	/*/-----------------------Palindrome
+	public static void main(String[] args) {
 	String str = "refeR";
 	String strRev = "";
 	//to reverse given string 
@@ -112,12 +123,12 @@ public static void main(String[] args) {
 	}
 	else {
 		System.out.println("Given string is not palindrome");
-	} */ 
+	} */
 	
 	
 	
 	
-	/*/----------------------print statement infinite times
+	//----------------------print statement infinite times
 	public static void subMethod() {
 		System.out.println("Hello");
 		subMethod();
@@ -125,22 +136,9 @@ public static void main(String[] args) {
 	public static void main(String[] args) {
 		subMethod();
 		
-	//Result: java.lang.StackOverflowError    */
+	//Result: java.lang.StackOverflowError    
 	
-	
-	/*/print statement using Recursion
-	static int count=0; //Global variable -- if not declare will get StackOverflowError
-	public static void subMethod() {
-		count++;
-		if(count<=5) {
-			System.out.println("Hello  "+count);
-			subMethod();
-		}		
+	 
+	 
 	}
-	public static void main(String[] args) {
-		subMethod(); */
-	
-	 
-	 
-}
 }
