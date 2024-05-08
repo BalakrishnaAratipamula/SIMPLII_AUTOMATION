fa3a757baddd22e3505bc043f75361cb8a552ebb
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-05-08 23:51:56 +0530

    Bala_08May'24

---
+++ AUTOMATION/Java_WD_Ex/Java_Examples2/ArrayOfObjects/InitializingUsing_SeparateMemberMethod.java
@@ -0,0 +1,47 @@
+package ArrayOfObjects;
+
+public class InitializingUsing_SeparateMemberMethod {
+	public static void main(String args[]) {
+
+		// Declaring an array of student
+		Student2[] arr;
+
+		// Allocating memory for 2 objects of type student
+		arr = new Student2[2];
+
+		// Creating actual student objects
+		arr[0] = new Student2();
+		arr[1] = new Student2();
+
+		// Assigning data to student objects
+		arr[0].setData(1701289270, "Satyabrata");
+		arr[1].setData(1701289219, "Omm Prasad");
+
+		// Displaying the student data
+		System.out.println("Student data in student arr 0: ");
+		arr[0].display();
+
+		System.out.println("Student data in student arr 1: ");
+		arr[1].display();
+	}
+}
+
+//Creating a Student class with
+//id and name as a attributes
+class Student2 {
+
+	public int id;
+	public String name;
+
+	// Method to set the data to student objects
+	public void setData(int id, String name) {	//////not using constructor
+		this.id = id;
+		this.name = name;
+	}
+
+	// display() method to display the student data
+	public void display() {
+		System.out.println("Student id is: " + id + " " + "and Student name is: " + name);
+		System.out.println();
+	}
+}
---
+++ AUTOMATION/Java_WD_Ex/src/Java_Examples/MethodHiding.java
@@ -0,0 +1,28 @@
+package Java_Examples;
+
+//Base Class
+class Complex {
+	public static void f1() {
+		System.out.println("f1 method of the Complex class is executed.");
+	}
+}
+
+//class child extend Demo class
+class Sample extends Complex {
+	public static void f1() {
+		System.out.println("f1 of the Sample class is executed.");
+	}
+}
+
+public class MethodHiding {
+	public static void main(String args[]) {
+		Complex d1 = new Complex();
+
+		// d2 is reference variable of class Demo that points to object of class Sample
+		Complex d2 = new Sample();
+
+		// But here method will be call using type of reference
+		d1.f1();
+		d2.f1();
+	}
+}
