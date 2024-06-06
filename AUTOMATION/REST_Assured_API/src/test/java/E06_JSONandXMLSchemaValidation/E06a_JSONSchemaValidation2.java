02900413a08a09dd912eeee19893a97f0251f1d5
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-06-06 21:26:33 +0530

    bala_06Jun'24

--- AUTOMATION/REST_Assured_API/src/test/java/E06_JSONandXMLSchemaValidation/E06a_JSONSchemaValidation2.java
+++ AUTOMATION/REST_Assured_API/src/test/java/E06_JSONandXMLSchemaValidation/E06a_JSONSchemaValidation2.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
 
-public class E06a_JSONSchemaValidation {
+public class E06a_JSONSchemaValidation2 {
 
 	//Response validation - validating the data in the response
 	//Schema Validation - focus on type of data we are getting - is nothing but whether data type is given prorply or not (like name is string, id is integer......)
@@ -18,7 +18,7 @@ public class E06a_JSONSchemaValidation {
 	@Test
 	void jsonSchemaValidation() {
 		// pre-requisites - convert JSON to JSON Schema - https://jsonformatter.org/json-to-jsonschema - store in any folder (F:\JSON Files\JsonSchema.json)
-		// pre-requisites run - jsonForSchema.json (F:\JSON Files\jsonForSchema.json) through cmd
+		// pre-requisites run - jsonForSchema3.json (F:\JSON Files\jsonForSchema.json) through cmd
 		// maintain (F:\JSON Files\JsonSchema.json) file in Resources folder
 		// N: json file must be in { "store":{ "book":[{},{},{}...]}} hierarchy and after converting json-jsonschema remove store part
 
@@ -27,6 +27,6 @@ public class E06a_JSONSchemaValidation {
 		.when()
 			.get("http://localhost:3000/store")	//F:\JSON Files\jsonForSchema.json
 		.then()
-			.assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("storeJsonSchema.json"));	//JsonSchema file name
+			.assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("studentsJsonSchema.json"));	//JsonSchema file name
 	}
 }
