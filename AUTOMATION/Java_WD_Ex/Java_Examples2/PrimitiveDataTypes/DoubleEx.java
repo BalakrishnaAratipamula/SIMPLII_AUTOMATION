a066b32cf81fb1737d43463aac81ef13034a60b2
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-06-10 21:49:32 +0530

    Bala_10Jun'24

---
+++ AUTOMATION/Java_WD_Ex/Java_Examples2/PrimitiveDataTypes/DoubleEx.java
@@ -0,0 +1,22 @@
+package PrimitiveDataTypes;
+
+public class DoubleEx {
+	public static void main(String[] args) { //https://www.javatpoint.com/double-keyword-in-java
+		Double dobj = new Double(23.45);
+		double d = dobj.doubleValue();
+		System.out.println(d);
+
+		d = 7.83684987683688;
+		System.out.println(d);
+
+		d = 50;
+		System.out.println(d);
+
+		d = 56.34f;
+		System.out.println(d);
+
+		d = 34f;
+		System.out.println(d);
+
+	}
+}
