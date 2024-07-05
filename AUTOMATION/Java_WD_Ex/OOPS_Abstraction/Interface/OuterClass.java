90836626a2fff8674f9a9bea4a5f8565e8b1eea5
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-07-05 21:03:34 +0530

    Bala_05Jul'24_2

---
+++ AUTOMATION/Java_WD_Ex/OOPS_Abstraction/Interface/OuterClass.java
@@ -0,0 +1,19 @@
+package Interface;
+
+abstract class AnonymousClassEx {	//https://youtu.be/NQLZhkdVN4Q?si=Ybj1oZo7HET85aBk
+	public abstract void display();
+}
+
+public class OuterClass {
+	public static void main(String[] args) {
+		AnonymousClassEx innerClass = new AnonymousClassEx() {
+			@Override
+			public void display() {
+				System.out.println("Anonymous inner class example");
+			}
+			
+		};
+		innerClass.display();
+		
+	}
+}
