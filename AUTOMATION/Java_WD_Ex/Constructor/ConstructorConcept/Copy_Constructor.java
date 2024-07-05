09723febd501d06a5cba5c6cc77999ff38d1477f
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-07-05 20:37:07 +0530

    Bala_05Jul'24

--- AUTOMATION/Java_WD_Ex/Constructor/ConstructorConcept/Copy_Constructor.java
+++ AUTOMATION/Java_WD_Ex/Constructor/ConstructorConcept/Copy_Constructor.java
@@ -5,11 +5,11 @@ public class Copy_Constructor {
 	String name;
 	int age;
 	
-	Copy_Constructor(String n, int a){
+	Copy_Constructor(String n, int a){	//constructor 1
 		this.name=n;
 		this.age=a;
 	}
-	Copy_Constructor(Copy_Constructor p){
+	Copy_Constructor(Copy_Constructor p){ //constructor 2
 		this.name = p.name;
 		this.age = p.age;
 		System.out.println(name);
@@ -17,8 +17,7 @@ public class Copy_Constructor {
 	}
 	
 	public static void main(String[] args) {
-		Copy_Constructor cc1 = new Copy_Constructor("john", 32);
-		Copy_Constructor cc2 = new Copy_Constructor(cc1);
-		
+		Copy_Constructor cc1 = new Copy_Constructor("john", 32); //constructor 1
+		Copy_Constructor cc2 = new Copy_Constructor(cc1);	//constructor 2
 	}
 }
