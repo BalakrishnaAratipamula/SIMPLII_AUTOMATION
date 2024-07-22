4c7aba13d2bb943da1cae113a82134d8ebfb2530
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-07-22 20:40:12 +0530

    Bala_22Jul'24

--- AUTOMATION/Java_WD_Ex/Java_Examples2/Mutable_nd_Immutable_Objects/MutableString.java
+++ AUTOMATION/Java_WD_Ex/Java_Examples2/Mutable_nd_Immutable_Objects/MutableString.java
@@ -4,13 +4,13 @@ public class MutableString {
 	public static void main(String[] args) {
 		
 		//StringBuffer
-		StringBuffer str1 = new StringBuffer("Java");
+		StringBuffer str1 = new StringBuffer("Java- ");
 		System.out.println("Value of str1 before change :" + str1);
 		str1.append(" Some text");
 		System.out.println("Value of str1 after change :" + str1);
 		
 		//StringBuilder
-		StringBuilder str2 = new StringBuilder("Demo");
+		StringBuilder str2 = new StringBuilder("Demo- ");
 		System.out.println("\nValue of str2 before change :" + str2);
 		str2.append(" Another text");
 		System.out.println("Value of str2 after change :" + str2);
---
+++ AUTOMATION/Java_WD_Ex/Java_Examples2/WapperClass/Convert_object_into_primitives.java
@@ -0,0 +1,13 @@
+package WapperClass;
+
+public class Convert_object_into_primitives {
+	public static void main(String args[]) {
+		// Converting Integer to int
+		Integer a = new Integer(3);
+		int i = a.intValue();// converting Integer to int explicitly
+		int j = a;// unboxing, now compiler will write a.intValue() internally
+
+		//DataType starts with UpperCase means object and DataType starts with LowerCase means PDT
+		System.out.println(a + " " + i + " " + j);
+	}
+}
