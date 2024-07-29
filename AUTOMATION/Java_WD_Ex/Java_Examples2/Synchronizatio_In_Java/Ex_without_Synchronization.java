df63f5d9e134d5f989e2897556a655388d163008
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-07-29 22:43:25 +0530

    Bala_29Jul'24

---
+++ AUTOMATION/Java_WD_Ex/Java_Examples2/Synchronizatio_In_Java/Ex_without_Synchronization.java
@@ -0,0 +1,50 @@
+package Synchronizatio_In_Java;
+
+class Table {
+	void printTable(int n) {// method not synchronized //MyThread1 'n=5' MyThread2 'n=100'
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(n * i);
+			try {
+				Thread.sleep(400);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+class MyThread1 extends Thread {
+	Table t;
+
+	MyThread1(Table t) {
+		this.t = t;
+	}
+
+	public void run() {
+		t.printTable(5);
+	}
+
+}
+
+class MyThread2 extends Thread {
+	Table t;
+
+	MyThread2(Table t) {
+		this.t = t;
+	}
+
+	public void run() {
+		t.printTable(100);
+	}
+}
+
+public class Ex_without_Synchronization {
+	public static void main(String args[]) {
+		Table obj = new Table();// only one object
+		MyThread1 t1 = new MyThread1(obj);
+		MyThread2 t2 = new MyThread2(obj);
+		System.out.println("Table without using Synchronized method");
+		t1.start();
+		t2.start();
+	}
+}
