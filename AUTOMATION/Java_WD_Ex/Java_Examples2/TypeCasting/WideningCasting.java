365146beca325aa24ce5b65b10dd5b027a651da8
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-08-02 23:27:10 +0530

    Bala_02Aug'24

--- AUTOMATION/Java_WD_Ex/Java_Examples2/TypeCasting/WideningCasting.java
+++ AUTOMATION/Java_WD_Ex/Java_Examples2/TypeCasting/WideningCasting.java
@@ -12,12 +12,27 @@ public class WideningCasting {
 	    System.out.println("double: "+myDouble);   // Output 9.0
 	} //*/
 	
-	public static void main(String args[]) {
+	/*public static void main(String args[]) {
         int i = 10;
         float f = i;
         
         System.out.println("int: " +i);
         System.out.println("float: " +f);
+	} //*/
+	
+	/*/String to int
+	public static void main(String args[]) {
+		String s = "200";
+		int i = Integer.parseInt(s);
+		
+		System.out.println(i);
+	} //*/
+	
+	//int to String  
+	public static void main(String args[]) {
+		int i = 200;
+		String str = String.valueOf(i);		
+		System.out.println(str);
 	}
 	
 }
--- AUTOMATION/Java_WD_Ex/src/Java_Examples/Parameters_vs_Arguments.java
+++ AUTOMATION/Java_WD_Ex/src/Java_Examples/Parameters_vs_Arguments.java
@@ -2,17 +2,15 @@ package Java_Examples;
 
 public class Parameters_vs_Arguments {
 	
-	// the variables a and b are Parameters
-	public static int multiply(int a, int b) {
+	public static int multiply(int a, int b) { // the variables a and b are Parameters
 		return a * b;
 	}
 
 	public static void main(String[] args) {
-		// the variables x and y are Arguments
 		int x = 2, y=5;
 		
-		int product = multiply(x, y); //(or) int product = multiply(2, 5);
-
+		int product = multiply(x, y); // the variables x and y are Arguments
+									  //(or) int product = multiply(2, 5);
 		System.out.println("PRODUCT IS: " + product);
 	}
 }
