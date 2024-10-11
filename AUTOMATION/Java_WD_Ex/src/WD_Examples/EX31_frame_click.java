e2bb6f0e7d4bc74cca741c36077f5022754eab83
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-10-11 19:19:12 +0530

    Bala_11Oct'24

--- AUTOMATION/Java_WD_Ex/src/WD_Examples/EX31_frame_click.java
+++ AUTOMATION/Java_WD_Ex/src/WD_Examples/EX31_frame_click.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class EX31_click_on_frame {
+public class EX31_frame_click {
 	public static void main(String[] args) throws Exception {
 		//to initialize browser
 		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
---
+++ AUTOMATION/Java_WD_Ex/src/WD_Examples/EX32b_NestedFrames.java
@@ -0,0 +1,83 @@
+package WD_Examples;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class EX32b_NestedFrames {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		FirefoxOptions ops = new FirefoxOptions();
+		FirefoxProfile profile = new FirefoxProfile();
+		ops.setProfile(profile);
+
+		driver = new FirefoxDriver(ops);
+		driver.get("https://demoqa.com/nestedframes");
+		driver.manage().window().maximize();
+
+		Path path = Paths.get("E:\\ExtensionLink\\ublock_origin-1.60.0.xpi");
+		((FirefoxDriver) driver).installExtension(path);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		// Number of Frames on a Page
+		framesCountGeneric("iframe", "page"); // (locateFrameByTagName, identificationForConsoleMsg)
+
+		// Switch to parent frame
+		driver.switchTo().frame("frame1");
+		// Number of Frames on Parent Frame
+		framesCountGeneric("iframe", "Parent Frame"); // (locateFrameByTagName, identificationForConsoleMsg)
+
+		// switch to child frame
+		driver.switchTo().frame(0); //index
+		// Number of Frames on Child Frame
+		framesCountGeneric("iframe", "Child Frame"); // (locateFrameByTagName, identificationForConsoleMsg)
+
+		// Switch back to parent frame
+		driver.switchTo().parentFrame();
+		// Number of Frames on Parent Frame
+		framesCountGeneric("iframe", "Parent Frame"); // (locateFrameByTagName, identificationForConsoleMsg)
+
+		// switch to child frame
+		driver.switchTo().frame(0); //index
+		// Number of Frames on Child Frame
+		framesCountGeneric("iframe", "Child Frame"); // (locateFrameByTagName, identificationForConsoleMsg)
+
+		//Switch to default content
+        driver.switchTo().defaultContent(); //Enter to main window from any frame (child or parent)
+
+        //Try to print the heading of the main page without swithcing
+        WebElement mainPageText=driver.findElement(By.xpath("//*[@id='framesWrapper']/div[1]"));
+        System.out.println("Main Page Text: \n"+mainPageText.getText());
+
+        System.out.println("============End============");
+		driver.quit();
+	}
+
+	static void parentFrameGeneric() {
+		// Locate the Element inside parent frame
+		WebElement frame1Element = driver.findElement(By.tagName("body"));
+
+		// Get the text for frame1 element
+		System.out.println("Frame1 is :" + frame1Element.getText());
+
+	}
+
+	static void framesCountGeneric(String locateFrameByTagName, String identificationForConsoleMsg) {
+		int countIframes = driver.findElements(By.tagName(locateFrameByTagName)).size();
+		System.out.println("Number of iFrames inside the " + identificationForConsoleMsg + " : " + countIframes);
+		if (identificationForConsoleMsg.equals("Parent Frame") || identificationForConsoleMsg.equals("Child Frame")) 
+			System.out.println("Frame Text: " + driver.findElement(By.tagName("body")).getText());
+	}
+}
