46251a1b1df24de5960930c55c9c0dccf417fa59
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2024-12-31 22:42:07 +0530

    Bala_31Dec'24

---
+++ AUTOMATION/Java_WD_Ex/OOPS_Abstraction/Interface_MarkerInterface/MerkerInterface_SerializableClass.java
@@ -0,0 +1,30 @@
+package Interface_MarkerInterface;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class SerializableClass implements Serializable {
+	String data = "serializable";
+	
+	public String getData() {
+		return data;
+	}
+}
+
+public class MerkerInterface_SerializableClass {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		SerializableClass obj = new SerializableClass();
+		
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("output.txt"))){
+			oos.writeObject(obj);
+			System.out.println("*****Check the doenloaded file in project folder*****");
+		}catch(Exception e) {
+			System.out.println("searialization failed: "+e);
+//			e.printStackTrace();
+//			System.out.println(e.getMessage());
+		}
+	}
+}
