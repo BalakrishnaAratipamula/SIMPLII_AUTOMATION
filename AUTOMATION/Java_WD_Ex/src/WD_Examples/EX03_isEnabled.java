fa004bd045339a83368af5dc6a46e9f586ef4f4f
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2025-02-18 22:18:41 +0530

    Bala_18Feb'25

--- AUTOMATION/Java_WD_Ex/src/WD_Examples/EX03_isEnabled.java
+++ AUTOMATION/Java_WD_Ex/src/WD_Examples/EX03_isEnabled.java
@@ -9,13 +9,12 @@ import org.openqa.selenium.WebElement;
 public class EX03_isEnabled {
 	public static void main(String[] args) throws Exception {
 		//to initialize browser
-		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://en-gb.facebook.com/");
 		driver.manage().window().maximize();
 
 		//to create reference object for "Sign Up" button
-		WebElement ele = driver.findElement(By.xpath("/html[position()=1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/form[1]/div[5]/a[1]"));
+		WebElement ele = driver.findElement(By.xpath("//a[text()='Create new account']"));
 
 		//to read visibleText
 		String str1 = ele.getText();
@@ -23,7 +22,7 @@ public class EX03_isEnabled {
 		System.out.println("object VisibleText is: "+str1);
 
 		//to click on Sign-up
-		if(ele.isEnabled()==true) {   //(or) refobj.isEnabled()
+		if(ele.isEnabled()) {   //(or) refobj.isEnabled() //if(ele.isEnabled()==true)
 			//to click on 'Create New Account'
 			ele.click();
 			System.out.println("Object is enabled");
