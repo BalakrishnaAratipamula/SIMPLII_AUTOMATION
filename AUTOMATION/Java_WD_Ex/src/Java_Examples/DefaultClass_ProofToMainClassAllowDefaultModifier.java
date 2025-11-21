9e5f09f10ebd1c01c672311fa87a330ab5b67a45
BalakrishnaAratipamula <balakrishna.mvn@gmail.com>
2025-11-21 22:55:44 +0530

    Bala_21Nov'25

---
+++ AUTOMATION/Java_WD_Ex/src/Java_Examples/DefaultClass_ProofToMainClassAllowDefaultModifier.java
@@ -0,0 +1,7 @@
+package Java_Examples;
+
+class DefaultClass_ProofToMainClassAllowDefaultModifier { //RC on package - new - class - modifier (package)
+	public static void main(String[] args) {
+		System.out.println("Default Class get executed");
+	}
+}
